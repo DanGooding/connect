f200bc6236d604f1ff40fc94a8698f565a60bc7a
Dan Gooding <dan.gooding2000@gmail.com>
2020-07-29 19:09:18 +0100

    Serve the built single page frontend

--- server/index.js
+++ server/index.js
@@ -5,8 +5,15 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const app = express();
+const path = require('path');
 const apiRouter = require('./api.js');
 
 app.use('/api', apiRouter);
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 app.listen(process.env.PORT);
