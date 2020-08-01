5f739a53c7999cc6ffc842d8afeb268f2feaf56e
Dan Gooding <dan.gooding2000@gmail.com>
2020-08-01 16:57:26 +0100

    Make marking more tolerant

---
+++ client/src/lcs.test.js
@@ -0,0 +1,23 @@
+
+import lcs from './lcs.js';
+
+it('returns zero for empty strings', () => {
+  expect(lcs('', '')).toBe(0);
+  expect(lcs('hello', '')).toBe(0);
+  expect(lcs('', 'world')).toBe(0);
+});
+
+it('gives length for equal arguments', () => {
+  expect(lcs('a', 'a')).toBe(1);
+  expect(lcs('abcdefghijklmnopqrstuvwxyz', 'abcdefghijklmnopqrstuvwxyz')).toBe(26);
+});
+
+it('is correct for substring', () => {
+  expect(lcs('banana', 'ana')).toBe(3);
+  expect(lcs('23456', '0123456789')).toBe(5);
+});
+
+it('is correct for non substring', () => {
+  expect(lcs('amputation', 'sprain')).toBe(4); // 'pain'
+});
+
---
+++ client/src/markConnectionGuess.js
@@ -0,0 +1,47 @@
+
+import lcs from './lcs.js';
+
+// return an array of all words & numbers
+// converted to lowercase and with apostrophes removed
+function getWords(sentence) {
+  const matches =
+    sentence
+      .toLowerCase()
+      .replace('\'', '')
+      .matchAll(/[a-z0-9]+/g);
+  return Array.from(
+    matches,
+    ([word]) => word);
+}
+
+// words longer than 2 letters that sill arent useful
+const stopWords = ['all', 'the', 'are', 'have', 'this', 'that', 'types', 'there', 'from', 'for', 'words', 'things'];
+
+// is this word useful in marking an answer ?
+function isUsefulWord(word) {
+  return word.length > 2 && !stopWords.includes(word);
+}
+
+// are these words considered similar enough to match
+// allows pluralisation and slightly different forms
+function areSimilar(s, t) {
+  if (s === t) return true;
+  const similarity = lcs(s, t) / max(s.length, t.length);
+  return similarity >= 0.7;
+}
+
+// does the guess (at what the connection is) match the answer
+function markConnectionGuess(guess, answer) {
+  const guessWords = getWords(guess).filter(isUsefulWord);
+  const answerWords = getWords(answer).filter(isUsefulWord);
+
+  for (const answerWord of answerWords) {
+    // this word must appear in the guess
+    if (!guessWords.some(guessWord => areSimilar(guessWord, answerWord))) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export default markConnectionGuess;
