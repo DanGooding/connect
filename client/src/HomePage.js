ed015732cd9c626168510b2a90f78d75f320a9ff
Dan Gooding <dan.gooding2000@gmail.com>
2020-08-02 20:46:00 +0100

    Add description to homepage

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -6,7 +6,11 @@ import WallListFetcher from './WallListFetcher';
 function HomePage() {
   return (
     <div className="home-container">
-      <h1 className="home-title">Connect</h1>
+      <div className="home-description">
+        <h1 className="home-title">Connect</h1>
+        <p>A site to play some of the connecting walls from the quiz show Only Connect.</p>
+        <p>Each wall has 16 jumbled up clues, that need to be sorted into 4 connected groups of 4.</p>
+      </div>
       <WallListFetcher />
     </div>
   );
