ddae6bbf2dd6dca794a33912166c16f4ae16380f
Dan Gooding <dan.gooding2000@gmail.com>
2020-09-22 22:15:07 +0100

    Improve homepage description

--- client/src/home/HomePage.js
+++ client/src/home/HomePage.js
@@ -8,8 +8,13 @@ function HomePage() {
     <div className="home-container">
       <div className="home-description">
         <h1 className="home-title">Connect</h1>
-        <p>A site to play some of the connecting walls from the quiz show Only Connect.</p>
-        <p>Each wall has 16 jumbled up clues, that need to be sorted into 4 connected groups of 4.</p>
+        <p>An unofficial, fan made site to play some of the connecting walls from the quiz show Only Connect.</p>
+        <p>
+          Each wall has 16 jumbled up clues, that need to be sorted into 4 connected groups of 4.
+          You have 2:30 to find all the groups, but once only two remain you'll have three lives, 
+          so be careful of red herrings!
+        </p>
+        <p>These questions are taken from <a href="https://ocdb.cc/">The Only Connect Database</a></p>
       </div>
       <WallListFetcher />
     </div>
