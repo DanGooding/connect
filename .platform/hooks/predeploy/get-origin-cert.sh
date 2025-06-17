1a5eb434fc5306a3240809483f3c7f95f2905974
Dan Gooding <dan.gooding2000@gmail.com>
2025-06-17 14:33:34 +0100

    Remove region specifier in ssm query

--- .platform/hooks/predeploy/get-origin-cert.sh
+++ .platform/hooks/predeploy/get-origin-cert.sh
@@ -4,14 +4,12 @@ certificate=/etc/pki/tls/certs/origin-server.crt
 private_key=/etc/pki/tls/certs/origin-server.key
 
 aws ssm get-parameter \
-  --region=eu-west-2 \
   --name cloudflare-signed-origin-certificate \
   --query Parameter.Value \
   --output text \
   > $certificate
 
 aws ssm get-parameter \
-  --region=eu-west-2 \
   --name cloudflare-signed-origin-private-key \
   --with-decryption \
   --query Parameter.Value \
@@ -21,5 +19,3 @@ aws ssm get-parameter \
 chmod 0444 $certificate
 chmod 0440 $private_key
 chown nginx $certificate $private_key
-
-cat $certificate
