2860cf331a849997014fd0b9b004c81cf24754f8
Dan Gooding <dan.gooding2000@gmail.com>
2025-06-17 10:10:17 +0100

    Add nginx config to support tls

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,6 @@ rm -f build.zip
 rsync -av server/* build --exclude node_modules --exclude .env --exclude .gitignore
 (cd client; npm run build)
 cp -r client/build build/static
+cp -r .platform build
 
 (cd build; zip -r ../build.zip .)
