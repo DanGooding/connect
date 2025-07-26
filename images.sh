131274e19a35b02ec799f9267fb10026c927780e
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-26 15:20:12 +0100

    Script to push images to ECR

---
+++ images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+set -x
+
+USER_ID=196481062593
+REGION=eu-west-2
+REPO_BASE_URL=$USER_ID.dkr.ecr.$REGION.amazonaws.com
+TAG=latest
+
+PROXY_PATH=connect-proxy
+API_SERVER_PATH=connect-api-server
+
+docker build .       -t $PROXY_PATH
+docker build server/ -t $API_SERVER_PATH
+
+aws ecr get-login-password \
+  --region $REGION \
+| docker login $REPO_BASE_URL \
+  --username AWS \
+  --password-stdin \
+
+for IMAGE_PATH in $PROXY_PATH $API_SERVER_PATH; do
+  docker tag $IMAGE_PATH:$TAG $REPO_BASE_URL/$IMAGE_PATH:$TAG
+  docker push $REPO_BASE_URL/$IMAGE_PATH:$TAG
+done
