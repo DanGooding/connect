497b9c96f6863bf3533657d6c90a3c1eac7856be
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-26 12:19:10 +0100

    Create VPC with eip for app server

--- main.tf
+++ main.tf
@@ -13,11 +13,52 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "connect-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs                  = ["eu-west-2a"]
+  private_subnets      = ["10.0.101.0/24"]
+  public_subnets       = ["10.0.1.0/24"]
+  private_subnet_names = ["connect-priv-1"]
+  public_subnet_names  = ["connect-pub-1"]
+
+  create_igw           = true
+  enable_dns_hostnames = true
+
+  vpc_tags = {
+    Name = "connect-vpc"
+  }
+  igw_tags = {
+    Name = "connect-igw"
+  }
+  tags = {
+    project_name = var.project_name
+  }
+}
+
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
 
   tags = {
-    Name = "connect-app-server"
+    Name         = "connect-app-server"
+    project_name = var.project_name
   }
+}
+
+resource "aws_eip" "public_ip" {
+  instance = aws_instance.app_server.id
+  domain   = "vpc"
+  tags = {
+    Name         = "connect-public-ip"
+    project_name = var.project_name
+  }
+  depends_on = [module.vpc]
 }
