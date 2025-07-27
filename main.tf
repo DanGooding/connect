d0dde5f335a0317cae86b431d5004dcf65629fdc
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-27 11:06:27 +0100

    Define fargate service running behind load balancer

--- main.tf
+++ main.tf
@@ -2,17 +2,6 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-// query machine image that matches this filer from AWS
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.19.0"
@@ -20,11 +9,11 @@ module "vpc" {
   name = "connect-vpc"
   cidr = "10.0.0.0/16"
 
-  azs                  = ["eu-west-2a"]
-  private_subnets      = ["10.0.101.0/24"]
-  public_subnets       = ["10.0.1.0/24"]
-  private_subnet_names = ["connect-priv-1"]
-  public_subnet_names  = ["connect-pub-1"]
+  azs                  = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  private_subnets      = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  public_subnets       = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
+  private_subnet_names = ["connect-priv-a", "connect-priv-b", "connect-priv-c"]
+  public_subnet_names  = ["connect-pub-a", "connect-pub-b", "connect-pub-c"]
 
   create_igw           = true
   enable_dns_hostnames = true
@@ -38,29 +27,6 @@ module "vpc" {
   tags = {
     project_name = var.project_name
   }
-}
-
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
-  subnet_id              = module.vpc.public_subnets[0]
-
-  tags = {
-    Name         = "connect-app-server"
-    project_name = var.project_name
-  }
-}
-
-resource "aws_eip" "public_ip" {
-  instance = aws_instance.app_server.id
-  domain   = "vpc"
-  tags = {
-    Name         = "connect-public-ip"
-    project_name = var.project_name
-  }
-  depends_on = [module.vpc]
 }
 
 resource "aws_ecr_repository" "api_server_container_repo" {
@@ -80,5 +46,134 @@ resource "aws_ecr_repository" "proxy_container_repo" {
   }
   tags = {
     project_name = var.project_name
+  }
+}
+
+module "cluster" {
+  source = "terraform-aws-modules/ecs/aws//modules/cluster"
+
+  name = "connect-ecs-cluster"
+  default_capacity_provider_strategy = {
+    FARGATE = {
+      weight = 100
+    }
+  }
+
+  tags = {
+    project_name = var.project_name
+  }
+}
+
+module "proxy_service" {
+  source = "terraform-aws-modules/ecs/aws//modules/service"
+
+  name        = "proxy-service"
+  cluster_arn = module.cluster.arn
+
+  cpu                        = 256 // 0.25 vCPU
+  memory                     = 512 // 512 GB
+  desired_count              = 1
+  deployment_maximum_percent = 200
+  enable_autoscaling         = false
+
+  // TODO: do not keep this - we should probably nat to a single public ip
+  assign_public_ip = true
+
+  // ssh in for debugging
+  enable_execute_command = true
+
+  container_definitions = {
+    proxy_container = {
+      cpu       = 256
+      memory    = 512
+      essential = true
+      image     = "${aws_ecr_repository.proxy_container_repo.repository_url}:latest"
+      portMappings = [{
+        containerPort = 80
+        protocol      = "tcp"
+      }]
+    }
+
+  }
+
+  // TODO: service connect
+
+  load_balancer = {
+    service = {
+      target_group_arn = module.alb.target_groups["proxy"].arn
+      container_name   = "proxy_container"
+      container_port   = 80
+    }
+  }
+
+  subnet_ids = module.vpc.public_subnets
+
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+
+  tags = {
+    Name         = "proxy-ecs-service"
+    project_name = var.project_name
+  }
+}
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name               = "connect-alb"
+  load_balancer_type = "application"
+
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+
+  // TODO: unset?
+  enable_deletion_protection = false
+
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = module.vpc.vpc_cidr_block
+    }
+  }
+
+  listeners = {
+    proxy = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_key = "proxy"
+      }
+    }
+  }
+
+  target_groups = {
+    proxy = {
+      protocol    = "HTTP"
+      port        = 80
+      target_type = "ip"
+
+      // ECS will attach services to the group dynamically
+      create_attachment = false
+
+      // TODO: health check
+    }
   }
 }
