ac1e8f45d6afa3032d5b1ba40d8b8a12bccfc300
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-27 22:14:37 +0100

    Remove unused static file serving from api service

--- server/index.js
+++ server/index.js
@@ -5,15 +5,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const app = express();
-const path = require('path');
 const apiRouter = require('./api.js');
 
 app.use('/api', apiRouter);
-
-app.use(express.static(path.join(__dirname, 'static')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static', 'index.html'));
-});
-
 
 app.listen(process.env.PORT);
