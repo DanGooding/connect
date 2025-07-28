928eb72ff4f82eeab733f86a496b05f93c315c64
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-28 11:10:41 +0100

    Support https only

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@ variable "project_name" {
   default     = "connect"
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
+variable "domain_certificate_arn" {
+  description = "certificate of public domain for the site"
   type        = string
-  default     = "t2.micro"
+  default     = "arn:aws:acm:eu-west-2:196481062593:certificate/4ece37fb-0fe4-49d9-8929-65354870ca46"
 }
