127c469ea468bbbf14209ec3ec3f2d737c107122
Dan Gooding <dan.gooding2000@gmail.com>
2025-07-29 10:38:35 +0100

    Action to run build+upload part of script

---
+++ local-build-push-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+USER_ID=196481062593
+REGION=eu-west-2
+REGISTRY=$USER_ID.dkr.ecr.$REGION.amazonaws.com
+
+aws ecr get-login-password \
+  --region $REGION \
+| docker login $REGISTRY \
+  --username AWS \
+  --password-stdin \
+
+(
+  export TAG="$(git rev-parse HEAD)"
+  export REGISTRY
+  ./build-push-images.sh
+)
